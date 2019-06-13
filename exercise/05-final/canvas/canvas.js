00c327d01bed3ca7c505ba28acc3e3d4feddc223
kevin823lin <kevin823lin@gmail.com>
2019-06-14 01:10:57 +0800

    final

--- exercise/05-final/canvas/canvas.js
+++ exercise/05-final/canvas/canvas.js
@@ -2,9 +2,12 @@
 var T;
 var c, ctx;
 var x, y;
-window.onload = function () {
+function setCanvas() {
     c = document.getElementById("myCanvas");
     ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, c.width, c.height);
     x = Math.floor(Math.random() * c.width);
     y = Math.floor(Math.random() * c.height);
     x -= x % 20;
